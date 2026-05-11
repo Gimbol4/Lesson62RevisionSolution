103b47d4c435652c9cce8aef9466b83f00f26afd
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-11 18:37:11 +0300

    solve 5 task

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -8,4 +8,28 @@
 #include "logic.h"
 
 void get_last_local_minimum(int** matrix, int n, int m, int* ii, int* jj) {
+    *ii = 0;
+    *jj = 0;
+
+    if (n <= 0 || m <= 0 || matrix == nullptr || (n == 1 && m ==1)) {
+        return;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (matrix[i] == nullptr) continue;
+        for (int j = 0; j < m; ++j) {
+            bool isLocalMin = true;
+            int current = matrix[i][j];
+
+            if (i > 0 && matrix[i - 1][j] <= current) isLocalMin = false;
+            if (isLocalMin && i < n - 1 && matrix[i + 1][j] <= current) isLocalMin = false;
+            if (isLocalMin && j > 0 && matrix[i][j - 1] <= current) isLocalMin = false;
+            if (isLocalMin && j < m - 1 && matrix[i][j + 1] <= current) isLocalMin = false;
+
+            if (isLocalMin) {
+                *ii = i + 1;
+                *jj = j + 1;
+            }
+        }
+    }
 }
