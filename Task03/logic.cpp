504593e45c1213c2783ace0abbf76a4d41a0c046
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-11 17:35:17 +0300

    solved 3 task

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -9,5 +9,29 @@
 #include "logic.h"
 
 int count_rows_with_more_positive_values(int** matrix, int n, int m) {
-	return 0;
+	if (n < 1 || m < 1 || matrix == nullptr) {
+		return 0;
+	}
+
+	int count_of_row = 0;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		int count_of_pos = 0;
+		int count_of_other = 0;
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] > 0) {
+				count_of_pos++;
+			}
+			else {
+				count_of_other++;
+			}
+		}
+		if (count_of_pos > count_of_other) {
+			count_of_row++;
+		}
+	}
+	return count_of_row;
 }
